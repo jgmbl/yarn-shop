611187b6a96e650032e33ccdcf1fefcba5ab2e04
jgmbl <joannagembel@gmail.com>
2024-02-01 15:48:49 +0100

    YARN-38 add basic basic TDD test

---
+++ src/test/java/pl/jgmbl/yarnshop/TestHomepageController.java
@@ -0,0 +1,28 @@
+package pl.jgmbl.yarnshop;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class TestHomepageController {
+    private static HomepageController homepageController;
+    private static MockMvc mockMvc;
+
+    @BeforeAll
+    public static void setup() {
+        homepageController = new HomepageController();
+        mockMvc = MockMvcBuilders.standaloneSetup(homepageController).build();
+    }
+
+    @Test
+    public void testDisplayHomepage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("homepage"));
+    }
+}
