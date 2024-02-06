5ac6158d4d1a14fbda60015bef7cc5a3a7d9dad3
jgmbl <joannagembel@gmail.com>
2024-02-06 13:53:29 +0100

    YARN-43 add displaying register2.html

--- src/main/java/pl/jgmbl/yarnshop/RegisterController.java
+++ src/main/java/pl/jgmbl/yarnshop/RegisterController.java
@@ -1,4 +1,10 @@
 package pl.jgmbl.yarnshop;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class RegisterController {
+    @GetMapping("/register")
+    public String displayRegisterPage() {
+        return "register2";
+    }
 }
