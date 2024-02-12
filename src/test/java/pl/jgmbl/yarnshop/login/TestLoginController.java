52f3b78ab28963b077a0c91fe5daa0718847d281
jgmbl <joannagembel@gmail.com>
2024-02-12 13:32:14 +0100

    YARN-47 refactor the code

--- src/test/java/pl/jgmbl/yarnshop/login/TestLoginController.java
+++ src/test/java/pl/jgmbl/yarnshop/login/TestLoginController.java
@@ -1,16 +1,20 @@
-package pl.jgmbl.yarnshop;
+package pl.jgmbl.yarnshop.login;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pl.jgmbl.yarnshop.User;
+import pl.jgmbl.yarnshop.login.LoginController;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class TestLoginController {
     private static LoginController loginController;
+//    private static User user;
     private static MockMvc mockMvc;
 
 
@@ -21,8 +25,8 @@ public class TestLoginController {
     }
 
     @Test
-    public void testDisplayLogin() throws Exception {
-        mockMvc.perform(get("/login"))
+    public void testDisplayLoggingPage()throws Exception {
+        mockMvc.perform(get("/login").with(user("user")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("login2"));
     }
