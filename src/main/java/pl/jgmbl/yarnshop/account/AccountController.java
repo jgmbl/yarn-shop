45fd23e23df2d1f0e9cae424e62dec033a0f26f0
jgmbl <joannagembel@gmail.com>
2024-02-21 15:40:11 +0100

    YARN-52 add basic account page Controller

---
+++ src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
@@ -0,0 +1,12 @@
+//package pl.jgmbl.yarnshop.account;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AccountController {
+//    @GetMapping("/account")
+//    public String displayAccountPage() {
+//        return "account2";
+//    }
+//}
