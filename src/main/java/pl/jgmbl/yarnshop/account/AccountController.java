b61ccf357c12dd5aa8699878b09f48693a3f2dcd
jgmbl <joannagembel@gmail.com>
2024-02-21 16:22:59 +0100

    YARN-52 fix the bug in account cotroller

--- src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
+++ src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
@@ -1,12 +1,12 @@
-//package pl.jgmbl.yarnshop.account;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class AccountController {
-//    @GetMapping("/account")
-//    public String displayAccountPage() {
-//        return "account2";
-//    }
-//}
+package pl.jgmbl.yarnshop.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+    @GetMapping("/account")
+    public String displayAccountPage() {
+        return "account2";
+    }
+}
