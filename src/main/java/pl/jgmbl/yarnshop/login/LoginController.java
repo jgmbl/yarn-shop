73339d44a13eb3cf1363bbff56919c31361acd98
jgmbl <joannagembel@gmail.com>
2024-03-03 19:20:51 +0100

    YARN-47 first not working version of checking button

--- src/main/java/pl/jgmbl/yarnshop/login/LoginController.java
+++ src/main/java/pl/jgmbl/yarnshop/login/LoginController.java
@@ -1,12 +1,21 @@
 package pl.jgmbl.yarnshop.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
     public String displayLoggingPage() {
         return "login2";
+    }
+
+    @PostMapping("/login")
+    public String submitLoginForm(@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
+
+        return "account2";
     }
 }
