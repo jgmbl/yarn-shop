c734534128acd4f03589c488744fb78bc42e7c1f
jgmbl <joannagembel@gmail.com>
2024-03-03 18:53:44 +0100

    YARN-48 delete user package

--- src/main/java/pl/jgmbl/yarnshop/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.jgmbl.yarnshop;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- src/main/java/pl/jgmbl/yarnshop/user/UserService.java
+++
@@ -1,4 +0,0 @@
-package pl.jgmbl.yarnshop;
-
-public class UserService {
-}
