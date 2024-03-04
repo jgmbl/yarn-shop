287f747ba19e0b6118d670622e04f6ae8ed9e7d5
jgmbl <joannagembel@gmail.com>
2024-03-04 18:12:13 +0100

    YARN-46 add equal method to submitLoginForm based on hardcode

--- src/main/java/pl/jgmbl/yarnshop/login/LoginController.java
+++ src/main/java/pl/jgmbl/yarnshop/login/LoginController.java
@@ -1,7 +1,9 @@
 package pl.jgmbl.yarnshop.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -11,8 +13,14 @@ public class LoginController {
         return "login2";
     }
 
+
     @PostMapping("/login")
-    public String submitLoginForm () {
-        return "redirect:/account";
+    public String submitLoginForm (@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
+        if (loginForm.getEmail().equals("admin@test.com") && loginForm.getPassword().equals("password")) {
+            return "redirect:/account";
+        }
+
+        model.addAttribute("InvalidCredentials", true);
+        return "redirect:/login";
     }
 }
