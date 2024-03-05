f474921a13ef748b2f2432c3d0eaa2eea9ff9d6f
jgmbl <joannagembel@gmail.com>
2024-03-05 15:48:03 +0100

    YARN-45 change Login to LoginCotroller and LoginService

--- src/main/java/pl/jgmbl/yarnshop/login/LoginController.java
+++ src/main/java/pl/jgmbl/yarnshop/login/LoginController.java
@@ -1,14 +1,25 @@
 package pl.jgmbl.yarnshop.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.jgmbl.yarnshop.user.User;
+import pl.jgmbl.yarnshop.user.UserRepository;
+
+import java.util.Optional;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    LoginService loginService;
+
     @GetMapping("/login")
     public String displayLoggingPage() {
         return "login2";
@@ -16,12 +27,19 @@ public class LoginController {
 
 
     @PostMapping("/login")
-    public String submitLoginForm (@ModelAttribute(name = "loginForm") User user, Model model) {
-        if (user.getEmail().equals("admin@test.com") && user.getPassword().equals("password")) {
-            return "redirect:/account";
-        }
+    public String submitLoginForm (@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
+        Optional<User> optionalUser = userRepository.findByEmail(loginForm.getEmail());
 
-        model.addAttribute("InvalidCredentials", true);
-        return "redirect:/login";
+        if (loginService.checkIfUserExists(loginForm.getEmail())) {
+            User user = optionalUser.get();
+
+            String submittedLoginForm = loginService.submitLoginForm(loginForm, user, model);
+
+            return submittedLoginForm;
+        } else {
+            model.addAttribute("InvalidCredentials", true);
+            return "redirect:/login";
+        }
     }
+
 }
--- src/main/java/pl/jgmbl/yarnshop/login/LoginService.java
+++ src/main/java/pl/jgmbl/yarnshop/login/LoginService.java
@@ -1,8 +1,39 @@
 package pl.jgmbl.yarnshop.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import pl.jgmbl.yarnshop.user.User;
+import pl.jgmbl.yarnshop.user.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class LoginService {
+    @Autowired
+    UserRepository userRepository;
 
+    public String submitLoginForm (LoginForm loginForm, User user, Model model) {
+        Optional<User> optionalUser = userRepository.findByEmail(loginForm.getEmail());
+
+        if (checkIfUserExists(user.getEmail())) {
+
+            User existingUser = optionalUser.get();
+
+            if (existingUser.getPassword().equals(loginForm.getPassword())) {
+                return "redirect:/account";
+            }
+        }
+
+        model.addAttribute("InvalidCredentials", true);
+        return "redirect:/login";
+    }
+
+    boolean checkIfUserExists (String email) {
+        Optional<User> doesUserExists = userRepository.findByEmail(email);
+
+        return doesUserExists.isPresent();
+    }
 }
+
+
