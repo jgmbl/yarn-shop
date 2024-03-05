3d0bb4cdc16a7f9c8fb58f218742e4f9ef8ae099
jgmbl <joannagembel@gmail.com>
2024-03-05 15:52:53 +0100

    YARN-45 add LoginForm

---
+++ src/main/java/pl/jgmbl/yarnshop/login/LoginForm.java
@@ -0,0 +1,25 @@
+package pl.jgmbl.yarnshop.login;
+
+public class LoginForm {
+    String email;
+    String password;
+
+    public LoginForm() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
