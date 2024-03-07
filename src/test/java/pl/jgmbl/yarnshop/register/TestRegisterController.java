a495883c280845b6dad274c2578774fa96113d95
jgmbl <joannagembel@gmail.com>
2024-03-07 11:16:35 +0100

    YARN-47 add testPasswordHashing

--- src/test/java/pl/jgmbl/yarnshop/register/TestRegisterController.java
+++ src/test/java/pl/jgmbl/yarnshop/register/TestRegisterController.java
@@ -1,12 +1,14 @@
 package pl.jgmbl.yarnshop.register;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import pl.jgmbl.yarnshop.login.LoginController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +19,11 @@ public class TestRegisterController {
     private static RegisterController registerController;
     @MockBean
     private static MockMvc mockMvc;
+    @Autowired
+    PasswordSecurity passwordSecurity;
+
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
 
 
     @BeforeEach
@@ -29,5 +36,16 @@ public class TestRegisterController {
         mockMvc.perform(get("/register"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("register2"));
+    }
+
+    @Test
+    public void testPasswordHashing() {
+        String hashedPassword = passwordSecurity.hashpassword("password");
+        String dehashedPassword = passwordEncoder.encode(hashedPassword);
+
+        boolean passwordMatches = passwordEncoder.matches("password", dehashedPassword);
+
+        Assertions.assertTrue(passwordMatches);
+
     }
 }
