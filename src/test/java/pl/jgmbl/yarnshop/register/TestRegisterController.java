e27c19df55ded52c72e3ab0477fa693bdbbaab6f
jgmbl <joannagembel@gmail.com>
2024-03-07 10:08:33 +0100

    YARN-47 add SpringBootTest

--- src/test/java/pl/jgmbl/yarnshop/register/TestRegisterController.java
+++ src/test/java/pl/jgmbl/yarnshop/register/TestRegisterController.java
@@ -1,23 +1,27 @@
-package pl.jgmbl.yarnshop;
+package pl.jgmbl.yarnshop.register;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pl.jgmbl.yarnshop.login.LoginController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@SpringBootTest
 public class TestRegisterController {
     private static RegisterController registerController;
+    @MockBean
     private static MockMvc mockMvc;
 
 
-    @BeforeAll
-    public static void setup() {
-        registerController = new RegisterController();
-        mockMvc = MockMvcBuilders.standaloneSetup(registerController).build();
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new RegisterController()).build();
     }
 
     @Test
