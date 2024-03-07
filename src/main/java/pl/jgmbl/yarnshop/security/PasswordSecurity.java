13a46bcfd35719dcb0b2ce79d4b8b13e76b0d105
jgmbl <joannagembel@gmail.com>
2024-03-07 12:54:16 +0100

    YARN-49 change change hasPasswordin PasswordSecurity

--- src/main/java/pl/jgmbl/yarnshop/security/PasswordSecurity.java
+++ src/main/java/pl/jgmbl/yarnshop/security/PasswordSecurity.java
@@ -1,4 +1,29 @@
 package pl.jgmbl.yarnshop.security;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
 public class PasswordSecurity {
+    public byte[] hashPassword (String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecureRandom random = new SecureRandom();
+
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+
+        byte[] saltAndHash = new byte[hash.length + salt.length];
+
+        System.arraycopy(hash, 0, saltAndHash, 0, hash.length);
+        System.arraycopy(salt, 0, saltAndHash, hash.length, salt.length);
+
+        return saltAndHash;
+    }
 }
