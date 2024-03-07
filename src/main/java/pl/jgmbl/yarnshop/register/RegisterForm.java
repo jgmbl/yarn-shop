b772a6c0262cc24c03ad7219f74374b7d71aeb34
jgmbl <joannagembel@gmail.com>
2024-03-07 10:55:38 +0100

    YARN-56 add contructor and geeters/setters to RegisterForm

--- src/main/java/pl/jgmbl/yarnshop/register/RegisterForm.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterForm.java
@@ -1,4 +1,34 @@
 package pl.jgmbl.yarnshop.register;
 
 public class RegisterForm {
+    String email;
+    String password;
+    String repeatedPassword;
+
+    public RegisterForm() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatedPassword() {
+        return repeatedPassword;
+    }
+
+    public void setRepeatedPassword(String repeatedPassword) {
+        this.repeatedPassword = repeatedPassword;
+    }
 }
