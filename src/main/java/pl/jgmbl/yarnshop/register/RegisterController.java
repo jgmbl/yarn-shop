21902730601a784e48a6fab09f8422495fa463df
jgmbl <joannagembel@gmail.com>
2024-03-08 14:33:44 +0100

    YARN-47 add basic method of registration in RegisterController

--- src/main/java/pl/jgmbl/yarnshop/register/RegisterController.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterController.java
@@ -2,11 +2,17 @@ package pl.jgmbl.yarnshop.register;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
     @GetMapping("/register")
     public String displayRegisterPage() {
         return "register2";
+    }
+
+    @PostMapping("/register")
+    public String registerUser() {
+        return "account2";
     }
 }
