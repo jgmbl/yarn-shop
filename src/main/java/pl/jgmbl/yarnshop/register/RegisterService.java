8bce95fff115e49c07d581b02cf6587640846a13
jgmbl <joannagembel@gmail.com>
2024-03-08 15:11:32 +0100

    YARN-45 refactor the code in RegisterService

--- src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
@@ -15,13 +15,17 @@ public class RegisterService {
         return password1.equals(password2);
     }
 
+    boolean isFormNotBlank (String property, String property1, String property2) {
+        return property != null && property1 != null && property2 != null;
+    }
+
     public String registerUser(RegisterForm registerForm, Model model) {
 
-        System.out.println(registerForm.getEmail() + " " + registerForm.getPassword() +
-                " " + registerForm.getConfirmpassword());
+        String email = registerForm.getEmail();
+        String password = registerForm.getPassword();
+        String confirmedPassword = registerForm.getConfirmpassword();
 
-        if (registerForm.getEmail() != null && registerForm.getPassword()
-                != null && registerForm.getConfirmpassword() != null) {
+        if (isFormNotBlank(email, password, confirmedPassword) && comparePasswords(password, confirmedPassword)) {
             return "redirect:/account";
         }
 
