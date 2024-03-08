dce891d0996d82c9961d8cd169caf30d29def414
jgmbl <joannagembel@gmail.com>
2024-03-08 14:58:44 +0100

    YARN-42 add redirection in Registration

--- src/main/java/pl/jgmbl/yarnshop/register/RegisterController.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterController.java
@@ -1,18 +1,24 @@
 package pl.jgmbl.yarnshop.register;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
+    @Autowired
+    RegisterService registerService;
+
     @GetMapping("/register")
     public String displayRegisterPage() {
         return "register2";
     }
 
     @PostMapping("/register")
-    public String registerUser() {
-        return "account2";
+    public String registerUser(@ModelAttribute(name = "registerForm") RegisterForm registerForm, Model model) {
+        return registerService.registerUser(registerForm, model);
     }
 }
--- src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
@@ -2,11 +2,30 @@ package pl.jgmbl.yarnshop.register;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import pl.jgmbl.yarnshop.user.UserRepository;
 
 @Service
 public class RegisterService {
+
     @Autowired
     UserRepository userRepository;
-    boolean
+
+    boolean comparePasswords (String password1, String password2) {
+        return password1.equals(password2);
+    }
+
+    public String registerUser(RegisterForm registerForm, Model model) {
+
+        System.out.println(registerForm.getEmail() + " " + registerForm.getPassword() +
+                " " + registerForm.getConfirmpassword());
+
+        if (registerForm.getEmail() != null && registerForm.getPassword()
+                != null && registerForm.getConfirmpassword() != null) {
+            return "redirect:/account";
+        }
+
+        model.addAttribute("InvalidCredentials", true);
+        return "register2";
+    }
 }
