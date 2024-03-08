0fdea1ca58a73fa1ff92b9616c99f154c35785af
jgmbl <joannagembel@gmail.com>
2024-03-08 11:22:31 +0100

    YARN-53 add RegisterService

--- src/main/java/pl/jgmbl/yarnshop/register/ComparePasswords.java
+++ src/main/java/pl/jgmbl/yarnshop/register/ComparePasswords.java
@@ -1,10 +1,7 @@
 package pl.jgmbl.yarnshop.register;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class ComparePasswords {
-    public boolean comparePasswords(String password1, String password2) {
-        return password1.equals(password2);
+    public boolean comparePasswords(String password, String password1) {
+        return password.equals(password1);
     }
 }
---
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
@@ -0,0 +1,12 @@
+package pl.jgmbl.yarnshop.register;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.jgmbl.yarnshop.user.UserRepository;
+
+@Service
+public class RegisterService {
+    @Autowired
+    UserRepository userRepository;
+    boolean
+}
