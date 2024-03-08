44b066ef93e000cd52233bdfbbd32f99cce4454b
jgmbl <joannagembel@gmail.com>
2024-03-08 15:28:28 +0100

    YARN-53 add doesUserExist to RegisterService

--- src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
@@ -3,7 +3,10 @@ package pl.jgmbl.yarnshop.register;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import pl.jgmbl.yarnshop.user.User;
 import pl.jgmbl.yarnshop.user.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class RegisterService {
@@ -19,13 +22,18 @@ public class RegisterService {
         return property != null && property1 != null && property2 != null;
     }
 
+    boolean doesUserExist(String email) {
+        return userRepository.findByEmail(email).isPresent();
+    }
+
     public String registerUser(RegisterForm registerForm, Model model) {
 
         String email = registerForm.getEmail();
         String password = registerForm.getPassword();
         String confirmedPassword = registerForm.getConfirmpassword();
 
-        if (isFormNotBlank(email, password, confirmedPassword) && comparePasswords(password, confirmedPassword)) {
+        if (isFormNotBlank(email, password, confirmedPassword) && comparePasswords(password, confirmedPassword)
+            && !doesUserExist(email)) {
             return "redirect:/account";
         }
 
