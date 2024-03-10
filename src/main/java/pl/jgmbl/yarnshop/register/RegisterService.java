a385645bc1377646b574997a201abe626645d15d
jgmbl <joannagembel@gmail.com>
2024-03-10 13:28:47 +0100

    YARN-53 add PasswordValidator to RegisterService

--- src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
+++ src/main/java/pl/jgmbl/yarnshop/register/RegisterService.java
@@ -3,15 +3,19 @@ package pl.jgmbl.yarnshop.register;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import pl.jgmbl.yarnshop.user.UserRepository;
 
 @Service
 public class RegisterService {
     @Autowired
     FormValidator formValidator;
 
-    public String registerUser(RegisterForm registerForm, Model model) {
-        if (formValidator.validator(registerForm.getEmail(), registerForm.getPassword(), registerForm.getConfirmpassword())) {
+    public String registerUser(RegisterForm registerForm, PasswordValidator passwordValidator, Model model) {
+
+        PasswordValidator lengthPasswordValidator = new LengthPasswordValidator();
+        PasswordValidator specialCharactersValidator = new SpecialCharactersValidator();
+
+        if (formValidator.validator(registerForm.getEmail(), registerForm.getPassword(), registerForm.getConfirmpassword()) &&
+            lengthPasswordValidator.validate(registerForm.getPassword()) && specialCharactersValidator.validate(registerForm.getPassword())) {
             return "redirect:/account";
         }
 
