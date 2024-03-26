a61a3a0cfa8be446649add0dbe3b2ca78c53d201
jgmbl <joannagembel@gmail.com>
2024-03-26 15:03:19 +0100

    YARN-66 add logout and checking if user is logged

--- src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
+++ src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
@@ -10,12 +10,25 @@ public class AccountController {
     @Autowired
     HttpSession httpSession;
 
+    @Autowired
+    AccountService accountService;
+
     @GetMapping("/account")
     public String displayAccountPage() {
         if (httpSession.getAttribute("username") != null) {
             return "account2";
         }
 
-        return "login2";
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logOutUser() {
+        if (httpSession.getAttribute("username") != null) {
+            accountService.endSession();
+            return "redirect:/login";
+        }
+
+        return "account2";
     }
 }
