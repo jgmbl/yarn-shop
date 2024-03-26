6bf1c9b43559becfe2ebf1367276ff7a3f25d5ff
jgmbl <joannagembel@gmail.com>
2024-03-26 15:27:19 +0100

    YARN-60 change navbar to log in/ my account

--- src/main/java/pl/jgmbl/yarnshop/homepage/HomepageController.java
+++ src/main/java/pl/jgmbl/yarnshop/homepage/HomepageController.java
@@ -1,12 +1,28 @@
 package pl.jgmbl.yarnshop.homepage;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomepageController {
+
+    @Autowired
+    HttpSession httpSession;
+
+
     @GetMapping("/")
-    public static String displayHomepage() {
+    public String displayHomepage(Model model) {
+        Object user = httpSession.getAttribute("username");
+
+        if (user != null) {
+            model.addAttribute("isLoggedIn", true);
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
+
         return "homepage";
     }
 }
