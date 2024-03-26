4fc8da6542ef04dfd594567bc68c1538c12cc927
jgmbl <joannagembel@gmail.com>
2024-03-26 14:38:19 +0100

    YARN-67 getmapping check if user is logged

--- src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
+++ src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
@@ -1,12 +1,21 @@
 package pl.jgmbl.yarnshop.account;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
+    @Autowired
+    HttpSession httpSession;
+
     @GetMapping("/account")
     public String displayAccountPage() {
-        return "account2";
+        if (httpSession.getAttribute("username") != null) {
+            return "account2";
+        }
+
+        return "login2";
     }
 }
