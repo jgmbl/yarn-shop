64398edb50377db825e700d814332aad4ca276ec
jgmbl <joannagembel@gmail.com>
2024-03-27 16:18:40 +0100

    YARN-63 add test of not logged user

--- src/test/java/pl/jgmbl/yarnshop/account/TestAccount.java
+++ src/test/java/pl/jgmbl/yarnshop/account/TestAccount.java
@@ -1,29 +1,33 @@
 package pl.jgmbl.yarnshop.account;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestAccount {
 
-    private static AccountController accountController;
-    private static MockMvc mockMvc;
-    
-    @BeforeAll
-    public static void setup() {
-        accountController = new AccountController();
-        mockMvc = MockMvcBuilders.standaloneSetup(accountController).build();
-    }
+    @Autowired
+    AccountController accountController;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
-    public void testDisplayAccountPage() throws Exception {
-        mockMvc.perform(get("/account"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("account2"));
+    public void testDisplayAccountPageUnauthenticated() throws Exception {
+        MockHttpSession mockHttpSession = new MockHttpSession();
+        mockHttpSession.setAttribute("username", null);
+
+        mockMvc.perform(get("/account").session(mockHttpSession))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
     }
 }
