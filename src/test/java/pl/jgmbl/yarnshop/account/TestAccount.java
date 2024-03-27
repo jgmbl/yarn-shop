d753287cf0d59bba4da9f9870efe5de1cb9a7d28
jgmbl <joannagembel@gmail.com>
2024-03-27 16:39:32 +0100

    YARN-63 add test of logging out

--- src/test/java/pl/jgmbl/yarnshop/account/TestAccount.java
+++ src/test/java/pl/jgmbl/yarnshop/account/TestAccount.java
@@ -1,5 +1,6 @@
 package pl.jgmbl.yarnshop.account;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +9,7 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -38,7 +40,21 @@ public class TestAccount {
         mockHttpSession.setAttribute("username", "admin@test.com");
 
         mockMvc.perform(get("/account").session(mockHttpSession))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/login"))
+                .andExpect(model().attribute("username", "admin@test.com"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testLogOutUser() throws Exception {
+        MockHttpSession mockHttpSession = new MockHttpSession();
+        mockHttpSession.setAttribute("username", "admin@test.com");
+
+        mockMvc.perform(get("/logout").session(mockHttpSession).with(csrf()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account2"));
+                .andExpect(view().name("login2"));
+
+        Assertions.assertTrue((Boolean) mockHttpSession.getAttribute("username"), (String) null);
     }
 }
