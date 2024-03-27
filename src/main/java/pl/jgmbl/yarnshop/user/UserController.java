c363e6d23841fb99713902015f473b49ef15d3e4
jgmbl <joannagembel@gmail.com>
2024-03-27 15:32:32 +0100

    YARN-62 refactor UserController and UserService

--- src/main/java/pl/jgmbl/yarnshop/user/UserController.java
+++ src/main/java/pl/jgmbl/yarnshop/user/UserController.java
@@ -1,19 +1,13 @@
 package pl.jgmbl.yarnshop.user;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
     private final UserService userService;
-
-    @Autowired
-    HttpSession httpSession;
 
     @Autowired
     public UserController(UserService userService) {
@@ -26,17 +20,4 @@ public class UserController {
 
         return ResponseEntity.ok(allUsers);
     }
-
-    @DeleteMapping("users")
-    public ResponseEntity<Void> deleteUser() {
-
-        boolean isUserDeleted = userService.deleteUser();
-
-        if (!isUserDeleted) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.noContent().build();
-    }
-
 }
--- src/main/java/pl/jgmbl/yarnshop/user/UserService.java
+++ src/main/java/pl/jgmbl/yarnshop/user/UserService.java
@@ -1,6 +1,5 @@
 package pl.jgmbl.yarnshop.user;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,23 +8,7 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    HttpSession httpSession;
-
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
-    }
-
-    public boolean deleteUser () {
-        String id = httpSession.getId();
-        Integer sessionId = Integer.parseInt(id);
-
-        if (!userRepository.existsById(sessionId)) {
-            return false;
-        }
-
-        userRepository.deleteById(sessionId);
-
-        return true;
     }
 }
