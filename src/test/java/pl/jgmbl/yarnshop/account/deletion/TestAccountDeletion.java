0ac6d9f0556b4f2573d1650dbed61994f76d4f6c
jgmbl <joannagembel@gmail.com>
2024-04-01 12:29:05 +0200

    YARN-63 add test of account deletion

--- src/test/java/pl/jgmbl/yarnshop/account/deletion/TestAccountDeletion.java
+++ src/test/java/pl/jgmbl/yarnshop/account/deletion/TestAccountDeletion.java
@@ -1,4 +1,50 @@
 package pl.jgmbl.yarnshop.account.deletion;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestAccountDeletion {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountDeletionService accountDeletionService;
+
+    @Autowired
+    private MockHttpServletRequest request;
+
+    @Autowired
+    MockHttpSession session;
+
+    @BeforeEach
+    public void setUp() {
+        session = new MockHttpSession();
+        session.setAttribute("username", "admin@test.com");
+
+
+        Mockito.when(accountDeletionService.deleteAccountBySessionId()).thenReturn(true);
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/accountdeletion")
+                        .session((MockHttpSession) Objects.requireNonNull(request.getSession())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
 }
