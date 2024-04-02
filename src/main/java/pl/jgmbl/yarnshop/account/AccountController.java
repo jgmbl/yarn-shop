a5c8633a401f44a81725fc8a4c589c28f70d2669
jgmbl <joannagembel@gmail.com>
2024-04-02 11:59:37 +0200

    YARN-60 add finding data by session id

--- src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
+++ src/main/java/pl/jgmbl/yarnshop/account/AccountController.java
@@ -3,16 +3,32 @@ package pl.jgmbl.yarnshop.account;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.jgmbl.yarnshop.Purchase;
+import pl.jgmbl.yarnshop.PurchaseRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AccountController {
     @Autowired
     HttpSession httpSession;
 
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+    @Autowired
+    AccountService accountService;
+
     @GetMapping("/account")
-    public String displayAccountPage() {
+    public String displayAccountPage(Model model) {
         if (isUserLoggedIn()) {
+            Integer accountId = accountService.returnSessionAccountId(httpSession.getAttribute("username").toString());
+
+            Optional<Purchase> purchases = accountService.findPurchasesByAccountId(accountId);
+
             return "account2";
         }
 
--- src/main/java/pl/jgmbl/yarnshop/account/AccountService.java
+++ src/main/java/pl/jgmbl/yarnshop/account/AccountService.java
@@ -1,7 +1,35 @@
 package pl.jgmbl.yarnshop.account;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.jgmbl.yarnshop.Purchase;
+import pl.jgmbl.yarnshop.PurchaseRepository;
+import pl.jgmbl.yarnshop.user.User;
+import pl.jgmbl.yarnshop.user.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class AccountService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HttpSession httpSession;
+
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+    public Optional<Purchase> findPurchasesByAccountId (Integer accountId) {
+        return purchaseRepository.findById(accountId);
+    }
+
+    public Integer returnSessionAccountId(String email) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+
+        Integer userId = optionalUser.map(User::getId).orElse(null);
+
+        return userId;
+    }
 }
