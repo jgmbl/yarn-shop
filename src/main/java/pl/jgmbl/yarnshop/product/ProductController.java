1e8434345cf42a3eab68b028448031e04e4f01e0
jgmbl <joannagembel@gmail.com>
2024-04-17 12:11:45 +0200

    YARN-72 display product pages

--- src/main/java/pl/jgmbl/yarnshop/product/ProductController.java
+++ src/main/java/pl/jgmbl/yarnshop/product/ProductController.java
@@ -1,12 +1,25 @@
 package pl.jgmbl.yarnshop.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class ProductController {
-    @GetMapping("/product")
-    public String displayProductPage() {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/product/{id}")
+    public String displayProductPage(@PathVariable Integer id, Model model) {
+        Optional<Yarn> yarnByIdOptional = productService.getYarn(id);
+        Yarn yarn = yarnByIdOptional.orElse(null);
+
+        model.addAttribute("yarnById", yarn);
+
         return "productpage";
     }
 }
