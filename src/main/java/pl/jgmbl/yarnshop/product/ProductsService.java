675de4c7fb5fe77f7f5ab6f0dca4fcbf769d83d0
jgmbl <joannagembel@gmail.com>
2024-04-17 12:44:24 +0200

    YARN-74 add getAllYarn method ProductsService

--- src/main/java/pl/jgmbl/yarnshop/product/ProductsService.java
+++ src/main/java/pl/jgmbl/yarnshop/product/ProductsService.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class ProductService {
+public class ProductsService {
     @Autowired
     private YarnRepository yarnRepository;
+
+    public Iterable<Yarn> getAllYarn() {
+        return yarnRepository.findAll();
+    }
 
     public Optional<Yarn> getYarn(Integer id) {
         return yarnRepository.findById(id);
