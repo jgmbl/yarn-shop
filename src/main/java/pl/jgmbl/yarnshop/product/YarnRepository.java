a1fb8a4f7cfa95e32e673ca69a966b77df440ba5
jgmbl <joannagembel@gmail.com>
2024-04-17 15:20:41 +0200

    YARN-75 add findByComposition method return List

--- src/main/java/pl/jgmbl/yarnshop/product/YarnRepository.java
+++ src/main/java/pl/jgmbl/yarnshop/product/YarnRepository.java
@@ -2,5 +2,8 @@ package pl.jgmbl.yarnshop.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface YarnRepository extends JpaRepository<Yarn, Integer> {
+    List<Yarn> findByComposition(String composition);
 }
