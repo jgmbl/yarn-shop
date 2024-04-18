bbde384863e9b71c7929dd704532ef227b9faba5
jgmbl <joannagembel@gmail.com>
2024-04-18 11:03:38 +0200

    YARN-77 add SearchForm and SearchController

---
+++ src/main/java/pl/jgmbl/yarnshop/search/SearchController.java
@@ -0,0 +1,7 @@
+package pl.jgmbl.yarnshop.search;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SearchController {
+}
---
+++ src/main/java/pl/jgmbl/yarnshop/search/SearchForm.java
@@ -0,0 +1,16 @@
+package pl.jgmbl.yarnshop.search;
+
+public class SearchForm {
+    String query;
+
+    public SearchForm() {
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
