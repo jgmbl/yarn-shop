79a05ce8a5925e364f04259503e46c3ccfbf1bc0
jgmbl <joannagembel@gmail.com>
2024-04-18 11:15:12 +0200

    YARN-77 add displaying search page

--- src/main/java/pl/jgmbl/yarnshop/search/SearchController.java
+++ src/main/java/pl/jgmbl/yarnshop/search/SearchController.java
@@ -1,7 +1,13 @@
 package pl.jgmbl.yarnshop.search;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SearchController {
+    
+    @GetMapping("/search")
+    public String displaySearchPage() {
+        return "search";
+    }
 }
