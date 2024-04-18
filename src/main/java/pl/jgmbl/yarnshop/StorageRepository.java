44a3c247c6f35cf3e0f12df5db7101e9bc61ba8e
jgmbl <joannagembel@gmail.com>
2024-04-18 15:43:09 +0200

    YARN-83 add findByYarnId method to StorageRepository

--- src/main/java/pl/jgmbl/yarnshop/StorageRepository.java
+++ src/main/java/pl/jgmbl/yarnshop/StorageRepository.java
@@ -2,5 +2,8 @@ package pl.jgmbl.yarnshop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StorageRepository extends JpaRepository<Storage, Integer> {
+    Optional<Storage> findByYarnId (Integer yarn_id);
 }
