42c3aa9304049f07f16d60f89f2ac1cd84f354cb
jgmbl <joannagembel@gmail.com>
2024-04-19 12:15:57 +0200

    YARN-82 temp add redirection to cart page from productpage/7

--- src/main/java/pl/jgmbl/yarnshop/cart/CartController.java
+++ src/main/java/pl/jgmbl/yarnshop/cart/CartController.java
@@ -1,7 +1,14 @@
 package pl.jgmbl.yarnshop.cart;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.jgmbl.yarnshop.product.Yarn;
 
 @Controller
 public class CartController {
+    @GetMapping("/cart")
+    public String displayCartPage() {
+        return "cartpage";
+    }
 }
